9d2ef111e9b3a601299268146ebd6646038a6600
skokosioulis <skokosioulis@gmail.com>
2025-08-03 09:13:13 +0300

    support external urls

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -51,8 +51,12 @@ class Media extends Model
 
     public function getUrlAttribute(): string
     {
+        if ($this->disk == 'external') {
+            return Storage::disk($this->disk)->url($this->file_name);
+        }
         return Storage::disk($this->disk)->url($this->path);
     }
+
 
     public function getHumanReadableSizeAttribute(): string
     {
@@ -63,7 +67,7 @@ class Media extends Model
             $bytes /= 1024;
         }
 
-        return round($bytes, 2).' '.$units[$i];
+        return round($bytes, 2) . ' ' . $units[$i];
     }
 
     public function getExtensionAttribute(): string
@@ -155,7 +159,7 @@ class Media extends Model
     {
         // Check collection-specific MIME types first
         $collectionConfig = config("media.collections.{$collection}");
-        if ($collectionConfig && isset($collectionConfig['accepts_mime_types']) && ! empty($collectionConfig['accepts_mime_types'])) {
+        if ($collectionConfig && isset($collectionConfig['accepts_mime_types']) && !empty($collectionConfig['accepts_mime_types'])) {
             return in_array($mimeType, $collectionConfig['accepts_mime_types']);
         }
 
@@ -172,7 +176,7 @@ class Media extends Model
     {
         // Check collection-specific MIME types first
         $collectionConfig = config("media.collections.{$collection}");
-        if ($collectionConfig && isset($collectionConfig['accepts_mime_types']) && ! empty($collectionConfig['accepts_mime_types'])) {
+        if ($collectionConfig && isset($collectionConfig['accepts_mime_types']) && !empty($collectionConfig['accepts_mime_types'])) {
             return $collectionConfig['accepts_mime_types'];
         }
 
